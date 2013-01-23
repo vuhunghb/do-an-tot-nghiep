2716b832122fc1c5ab4d8196575357026957ecb6
hoangtuan877 <hoangtuan877@dc68d640-436e-7358-9778-aebe7c500bc5>
2013-01-23 16:18:40 +0000

    [DATN]update client code

--- source/IRestads/src/com/irestads/SettingsActivity.java
+++ source/IRestads/src/com/irestads/SettingsActivity.java
@@ -5,9 +5,13 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 @SuppressLint("NewApi")
 public class SettingsActivity extends Activity {
+	FragmentManager fragmentManager;
 
 	@SuppressLint("NewApi")
 	@Override
@@ -15,13 +19,34 @@ public class SettingsActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
-		FragmentManager fragmentManager = getFragmentManager();
-		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		
+		fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager
+				.beginTransaction();
+
 		SettingsUpdateFragment updateFragment = new SettingsUpdateFragment();
 		fragmentTransaction.replace(R.id.settings_content, updateFragment);
 		fragmentTransaction.commit();
-		
+	}
+
+	public void onClickSettingButton(View v) {
+		LinearLayout layout = (LinearLayout) v;
+		int id = layout.getId();
+		FragmentTransaction fragmentTransaction = fragmentManager
+				.beginTransaction();
+
+		switch (id) {
+		case R.id.settings_btn_update:
+			SettingsUpdateFragment updateFragment = new SettingsUpdateFragment();
+			fragmentTransaction.replace(R.id.settings_content, updateFragment);
+			break;
+		case R.id.settings_btn_connect:
+			SettingsConnectFragment connectFragment = new SettingsConnectFragment();
+			fragmentTransaction.replace(R.id.settings_content, connectFragment);
+			break;
+		default:
+			break;
+		}
+		fragmentTransaction.commit();
 	}
 	
 }
---
+++ source/IRestads/src/com/irestads/util/StogeSettingsUtil.java
@@ -0,0 +1,64 @@
+package com.irestads.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
+
+import android.os.Environment;
+
+import com.irestads.model.SettingsModel;
+
+public class StogeSettingsUtil {
+	File sdFile = Environment.getExternalStorageDirectory();
+	String folderName = "iRestAds";
+
+	public void writeSettings(SettingsModel settings) {
+		File diretory = new File(sdFile.getAbsoluteFile() + "/" + folderName);
+		if (diretory.exists() != true || diretory.isDirectory() == false) {
+			diretory.mkdirs();
+		}
+		
+		try {
+			File file = new File(diretory, "settings.obj");
+			FileOutputStream fileOutputStream = new FileOutputStream(file,
+					false);
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+					fileOutputStream);
+
+			objectOutputStream.writeObject(settings);
+			objectOutputStream.flush();
+			objectOutputStream.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public SettingsModel readSettings() {
+		SettingsModel settingsModel = new SettingsModel();
+		File file = new File(sdFile.getAbsoluteFile() + "/" + folderName + "/"
+				+ "settings.obj");
+		if (file.exists() != true) {
+			this.writeSettings(settingsModel);
+		} else {
+			try {
+				FileInputStream fileInputStream = new FileInputStream(file);
+				ObjectInputStream objectInputStream = new ObjectInputStream(
+						fileInputStream);
+				settingsModel = (SettingsModel) objectInputStream.readObject();
+				objectInputStream.close();
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return settingsModel;
+	}
+}
